992454853957dc770a40d77f138370b0aa97ff98
Alejandro Irimia <rimax_86@hotmail.com>
2016-10-12 20:15:51 +0200

    Agregando JavaScript en el cliente

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var numeros = [400, 200,1,-23];
+
+var numerosMas1 = numeros.map(function(numero){
+  return numero + 1;
+});
+
+var numerosMas12015 = numeros.map(n => n + 1)
+
+console.log(numerosMas1);
+console.log(numerosMas12015);
