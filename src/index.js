730b686ad5d12da2073f1a0a3a9cf7c13b18d551
Alejandro Irimia <rimax_86@hotmail.com>
2016-10-18 20:46:12 +0200

    Automatizando el build

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ var page = require('page');
 var main = document.getElementById('main-container');
 
 page('/', function(ctx, next){
-  main.innerHTML = 'Home <a href="/signup">signup</a>';
+  main.innerHTML = 'Home <a href="/signup">Signup</a>';
 });
 
 page('/signup', function(ctx, next){
-  main.innerHTML = 'Signup <a href="/">home</a>';
+  main.innerHTML = 'Signup <a href="/">Home</a>';
 });
 
 page();
